36e5e6e00d765591e967e44209de826e34185fe9
Serina Ko <serinako08@gmail.com>
2018-09-23 22:21:55 +1000

    call the getState method on the created store to return the current state of my app

--- src/HelloSerina.js
+++ src/HelloSerina.js
@@ -3,7 +3,7 @@ import React from 'react';
 const HelloSerina = ({ tech }) => {
   return (
     <div className="hello-serina">
-      Hello Serina <span className="hello-serina__tech">{tech}!</span>
+      Hey Serina <span className="hello-serina__tech">{tech}!</span>
     </div>
   );
 };
