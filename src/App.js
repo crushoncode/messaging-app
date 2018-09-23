2ab2f3321090bfbab87aa0c10e5883a01b1c7fa4
Serina Ko <serinako08@gmail.com>
2018-09-23 22:19:58 +1000

    make createStore factory function and pass in an inital state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import HelloSerina from './HelloSerina';
+import reducer from './reducers';
+import { createStore } from 'redux';
+
+const initialState = { tech: 'ganbate!!' };
+const store = createStore(reducer, initialState);
 
 class App extends Component {
-  state = {
-    tech: ' ganbate!!'
-  };
   render() {
-    return <HelloSerina tech={this.state.tech} />;
+    return <HelloSerina tech={store.getState().tech} />;
   }
 }
 
